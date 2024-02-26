f6511fb0a954a2f53dbaddff31a215cc75ddae7b
Wang Xinming <wxm2002@tju.edu.cn>
2024-02-26 09:17:19 +0800

    ch7-05

--- notebooks/ch07/models.py
+++ notebooks/ch07/models.py
@@ -58,6 +58,195 @@ class LeNet(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+# LeNet_BN
+class LeNet_BN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.net = nn.Sequential(nn.Conv2d(self.in_channels, 6, kernel_size=5, padding=2), 
+                              nn.BatchNorm2d(6),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Conv2d(6, 16, kernel_size=5), 
+                              nn.BatchNorm2d(16),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Flatten(),
+                              nn.Linear(16 * 5 * 5, 120), 
+                              nn.Sigmoid(),
+                              nn.Linear(120, 84),
+                              nn.Sigmoid(),
+                              nn.Linear(84, self.num_classes))
+        
+    def forward(self, x):
+        return self.net(x)
+
+
+# LeNet_BN2
+class LeNet_BN2(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.net = nn.Sequential(nn.Conv2d(self.in_channels, 6, kernel_size=5, padding=2), 
+                              nn.BatchNorm2d(6),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Conv2d(6, 16, kernel_size=5), 
+                              nn.BatchNorm2d(16),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Flatten(),
+                              nn.Linear(16 * 5 * 5, 120), 
+                              nn.BatchNorm1d(120),
+                              nn.Sigmoid(),
+                              nn.Linear(120, 84),
+                              nn.BatchNorm1d(84),
+                              nn.Sigmoid(),
+                              nn.Linear(84, self.num_classes))
+        
+    def forward(self, x):
+        return self.net(x)
+
+# LeNet_all_BN
+class LeNet_all_BN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.net = nn.Sequential(nn.Conv2d(self.in_channels, 6, kernel_size=5, padding=2),  
+                                 nn.BatchNorm2d(6),
+                                 nn.Sigmoid(),
+                                 nn.BatchNorm2d(6),
+                                 nn.AvgPool2d(kernel_size=2, stride=2),
+                                 nn.BatchNorm2d(6),
+                                 nn.Conv2d(6, 16, kernel_size=5),
+                                 nn.BatchNorm2d(16),
+                                 nn.Sigmoid(),
+                                 nn.BatchNorm2d(16),
+                                 nn.AvgPool2d(kernel_size=2, stride=2),
+                                 nn.BatchNorm2d(16),
+                                 nn.Flatten(),
+                                 nn.Linear(16 * 5 * 5, 120),
+                                 nn.Sigmoid(),
+                                 nn.BatchNorm1d(120),
+                                 nn.Linear(120, 84),
+                                 nn.BatchNorm1d(84),
+                                 nn.Sigmoid(),
+                                 nn.BatchNorm1d(84),
+                                 nn.Linear(84, 10),
+                                 nn.BatchNorm1d(10)
+                                 )
+        
+    def forward(self, x):
+        return self.net(x) 
+
+
+# diy Batch_Norm layer to replace nn.BatchNorm2d 
+def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
+    
+    # 通过is_grad_enabled来判断当前模式是训练模式还是预测模式
+    if not torch.is_grad_enabled():
+        # 如果是在预测模式下，直接使用传入的移动平均所得的均值和方差
+        X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
+    else:
+        # 限制X的维度
+        assert len(X.shape) in (2, 4)
+        if len(X.shape) == 2:
+            # 使用全连接层的情况，计算特征维上的均值和方差
+            mean = X.mean(dim=0)
+            var = ((X - mean) ** 2).mean(dim=0)
+        else:
+            # 使用二维卷积层的情况，计算通道维上（axis=1）的均值和方差。
+            # 这里我们需要保持X的形状以便后面可以做广播运算
+            mean = X.mean(dim=(0, 2, 3), keepdim=True)
+            var = ((X - mean) ** 2).mean(dim=(0, 2, 3), keepdim=True)
+        # 训练模式下，用当前的均值和方差做标准化
+        X_hat = (X - mean) / torch.sqrt(var + eps)
+        
+        # 更新移动平均的均值和方差
+        moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
+    Y = gamma * X_hat + beta  # 缩放和移位
+    return Y, moving_mean.data, moving_var.data
+
+class BatchNorm(nn.Module):
+    # num_features：完全连接层的输出数量或卷积层的输出通道数。
+    # num_dims：2表示完全连接层，4表示卷积层
+    def __init__(self, num_features, num_dims, freeze=False):
+        super().__init__()
+        if num_dims == 2:
+            shape = (1, num_features)
+        else:
+            shape = (1, num_features, 1, 1)
+        # 参与求梯度和迭代的拉伸和偏移参数，分别初始化成1和0
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.beta = nn.Parameter(torch.zeros(shape))
+        # 非模型参数的变量初始化为0和1
+        self.moving_mean = torch.zeros(shape)
+        self.moving_var = torch.ones(shape)
+        self.freeze = freeze
+            
+        
+
+    def forward(self, X):
+        if self.freeze:
+            m = 1.0
+        else:
+            m = 0.9
+        # 如果X不在显存上，将moving_mean和moving_var复制到X所在显存上
+        if self.moving_mean.device != X.device:
+            self.moving_mean = self.moving_mean.to(X.device)
+            self.moving_var = self.moving_var.to(X.device)
+        # 保存更新过的moving_mean和moving_var
+        Y, self.moving_mean, self.moving_var = batch_norm(
+            X, self.gamma, self.beta, self.moving_mean,
+            self.moving_var, eps=1e-5, momentum=m)
+        return Y
+
+class LeNet_BN_freeze(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.net = nn.Sequential(nn.Conv2d(self.in_channels, 6, kernel_size=5, padding=2), 
+                              BatchNorm(6, 2, freeze=True),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Conv2d(6, 16, kernel_size=5), 
+                              BatchNorm(16, 2, freeze=True),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Flatten(),
+                              nn.Linear(16 * 5 * 5, 120), 
+                              nn.Sigmoid(),
+                              nn.Linear(120, 84),
+                              nn.Sigmoid(),
+                              nn.Linear(84, self.num_classes))
+
+class LeNet_BN2_freeze(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.net = nn.Sequential(nn.Conv2d(self.in_channels, 6, kernel_size=5, padding=2), 
+                              BatchNorm(6, 2, freeze=True),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Conv2d(6, 16, kernel_size=5), 
+                              BatchNorm(16, 2, freeze=True),
+                              nn.Sigmoid(),
+                              nn.AvgPool2d(kernel_size=2, stride=2),
+                              nn.Flatten(),
+                              nn.Linear(16 * 5 * 5, 120), 
+                              BatchNorm(120, 1, freeze=True),
+                              nn.Sigmoid(),
+                              nn.Linear(120, 84),
+                              BatchNorm(84, 1, freeze=True),
+                              nn.Sigmoid(),
+                              nn.Linear(84, self.num_classes))
+
 # LeNetPro: use ReLU instead of Sigmoid; append dropout layer after linear layer
 class LeNetPro(nn.Module):
     def __init__(self, in_channels=1, num_classes=10) -> None:
